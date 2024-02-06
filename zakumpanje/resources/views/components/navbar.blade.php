38b27a2243ee14fed66cd72126ebd0128a4abcda
Takondwa Kapyola <105532335+takondwamw@users.noreply.github.com>
2024-02-06 15:15:11 +0200

    update

---
+++ zakumpanje/resources/views/components/navbar.blade.php
@@ -0,0 +1,44 @@
+
+<nav class="navbar navbar-expand-lg">
+    <div class="container">
+        <a class="navbar-brand" href="index.html">
+                 <img src="zaKUMPANJE-WEB-LOGO~2.png"width="px"height="60" alt=""> 
+        </a>
+
+         <a href="Search.html">
+            <button class="button_1" type="submit">Search</button>
+         </a>
+       
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav align-items-lg-center ms-auto me-lg-5">
+                <li class="nav-item">
+                    <a class="nav-link click-scroll" href="index.html">Home</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link click-scroll" href="New releases.html">New Releases</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link click-scroll" href="Hot Songs.html">Hot Songs</a>
+                </li>
+    
+                <li class="nav-item">
+                    <a class="nav-link click-scroll" href="Hot artist.html">Hot Artists</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link click-scroll" href="#section_6">Get to Know</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link click-scroll" href="#section_7">contact us</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
